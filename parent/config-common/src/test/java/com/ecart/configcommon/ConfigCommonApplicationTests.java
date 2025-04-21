9d16021b3f331849e9b4ceac44cb90f67131680d
bahadirmac <bcacuner@gmail.com>
2025-04-21 14:08:08 +0300

    update authservice

---
+++ parent/config-common/src/test/java/com/ecart/configcommon/ConfigCommonApplicationTests.java
@@ -0,0 +1,11 @@
+package com.ecart.configcommon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigCommonApplicationTests {
+
+  @Test
+  void contextLoads() {}
+}
